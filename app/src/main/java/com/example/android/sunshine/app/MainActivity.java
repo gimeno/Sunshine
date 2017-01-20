0a91de7faf7f09793bf0e23a69fb9e2539c84921
Roberto Gimeno <r.gimenocallejas@gmail.com>
2017-01-20 00:04:38 +0000

    Move ForecastFragment to a new file
    Added new logic to use AsyncTask

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -1,19 +1,10 @@
 package com.example.android.sunshine.app;
 
 
-
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,38 +16,10 @@ public class MainActivity extends AppCompatActivity {
         if (savedInstanceState == null) {
             FragmentManager fragmentManager = getSupportFragmentManager();
             FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-            fragmentTransaction.add(R.id.container, new PlaceholderFragment());
+            fragmentTransaction.add(R.id.container, new ForecastFragment());
             fragmentTransaction.commit();
         }
 
-    }
-
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-
-            ArrayList<String> datesWeather = new ArrayList<>();
-
-            datesWeather.add("Today - Sunny - 88/63");
-            datesWeather.add("Tomorrow - Foggy - 70/46");
-
-            ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(), R.layout.list_item_forecast, R.id.list_item_forecast_textview, datesWeather);
-
-            // Inflate the layout for this fragment
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-
-            ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
-
-            listView.setAdapter(adapter);
-
-            return rootView;
-        }
     }
 }
 
